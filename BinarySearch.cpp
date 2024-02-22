0769407edbb123f3d3e15e415717f4d4560f1850
Shivankit Agarwal <shivankit1210@gmail.com>
2024-02-22 15:08:05 +0530

    update

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,14 +1,41 @@
-#include<iostream>
- using namespace std;
+#include <iostream>
+using namespace std;
 
+int BinarySearch(int arr[], int ele, int n)
+{
+    int beg = 0, end = n - 1, mid = (end + beg) / 2;
 
- 
+    while (beg <= end)
+    {
 
- int main()
- {
-    int n;
-    cout<<"Enter Size of Array";
-    cin>>n;
-    int arr[n];
-    
- }
+        if (arr[mid] == ele)
+        {
+            return mid;
+        }
+        else if (arr[mid] < ele)
+        {
+            beg = mid + 1;
+        }
+        else if (arr[mid] > ele)
+        {
+            end = mid - 1;
+        }
+        return -1;
+    }
+}
+
+int main()
+{
+    int arr[100], ele;
+    int pos;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Enter Element to be Searched for";
+    cin >> ele;
+    pos = BinarySearch(arr, ele, n);
+    if(pos!=0){
+        cout<<"Element is found at index "<<pos;
+    }
+    else
+    cout<<"Element is  not present in the array";
+    return 0;
+}
